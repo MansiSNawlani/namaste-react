198b2cedb0db1615a4c1e5a62293d10829b4f49c
MansiSNawlani <mansishankarnawlani@gmail.com>
2023-11-05 18:30:10 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!");
 console.log(heading); //object
 
 const element = React.createElement("div", { id: "parent" }, [
-    React.createElement("h1", { id: "child1" },
+    React.createElement("div", { id: "child1" },
     [
-        React.createElement("h1", {}, "I am a h1 tag"),
-        React.createElement("h2", {}, "I am a h2 tag"),
+        React.createElement("h1", {}, "This is Namaste React"),
+        React.createElement("h2", {}, "sdhgfjgdsjfgk"),
     ]),
-    React.createElement("h1", { id: "child2" },
+    React.createElement("div", { id: "child2" },
     [
         React.createElement("h1", {}, "I am a h1 tag"),
         React.createElement("h2", {}, "I am a h2 tag"),
---
+++ README.md
@@ -0,0 +1,18 @@
+# Namaste React 
+
+# Parcel
+- DEV Build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent hashing
+- Code spliting
+- Differential bundling - support older browsers
+- Diagnostic 
+- Error Handling
+- HTTP's
